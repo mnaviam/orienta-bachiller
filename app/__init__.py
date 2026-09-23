06528f02b0fd5d8c59a4c279b3ea935331587dcd
mnaviam <mnaviam@gmail.com>
2026-09-23 12:42:48 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,15 @@ from config import Config
 from app.models.database import init_db
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(base_dir, 'templates'),
+        static_folder=os.path.join(base_dir, 'static')
+    )
+    
+#    app = Flask(__name__)
     app.config.from_object(config_class)
 
     # Inicializar Base de Datos con tablas y datos semilla
